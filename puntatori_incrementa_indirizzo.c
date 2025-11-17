9db5ee87118b0934838592e3a81ac3f9f00db779
Marcosac1 <marcosaccone8@gmai.com>
2025-11-17 16:37:27 +0100

    Add implementation of incrementa function in puntatori_incrementa_indirizzo.c

---
+++ puntatori_incrementa_indirizzo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void incrementa(int*n){
+    *n=*n+1;
+    printf("n contiene l'indirizzo di x %p\n", (void*)n);
+    printf("n contiene l'indirizzo di n e'%p\n", (void*)&n);
+
+}
+
+int main() {
+    int x=5;
+    incrementa(&x);
+    printf("l'indirizzo e diventato -> %d \n", x);
+    printf("l'indirizzo di x e' -> %p \n", (void*)&x);
+    
+    return 0;
+}
